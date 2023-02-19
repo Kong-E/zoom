dda7d00a1e552e4f67fc9d2dd57a395c2c75f8a5
Kong-E <kng001016@gmail.com>
2023-02-20 07:36:04 +0900

    #1 CHAT WITH WEBSOCKETS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import livereloadMiddleware from "connect-livereload";
 import livereload from "livereload";
 import express from "express";
@@ -22,4 +24,31 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 2개가 같은 port에 있길 원해서 이렇게 한거고 wss만 만들어도 ok
+const server = http.createServer(app);
+// http 서버위에 ws 서버를 만듦
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
